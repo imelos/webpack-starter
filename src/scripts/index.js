56f010ac931b12597b4881f0b8eb8ac3f6ce64e7
pro <imelose@gmail.com>
2019-02-28 15:56:38 +0200

    +

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,25 @@
 import '../styles/index.scss';
+import Swiper from 'swiper';
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
 console.log('webpack starterkit');
 console.log('webpack starterkit');
