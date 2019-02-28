2a763e376887a684d21ff14a3fc8731bee2d0316
pro <imelose@gmail.com>
2019-02-28 16:28:57 +0200

    +

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,17 +8,8 @@ var mySwiper = new Swiper ('.swiper-container', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    type: 'bullets',
+    clickable: true,
   },
 })
 console.log('webpack starterkit');
